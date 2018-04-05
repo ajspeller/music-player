18f895007ee8add1f74d85a2981fdf78c065e872
AJ Speller <ajspeller@gmail.com>
2018-04-05 19:49:57 -0400

    Create the list of songs
    Create the list of songs using javascript calling the methods createElement, createTextNode, and appendChild.

--- music.js
+++ music.js
@@ -1,1 +1,22 @@
 console.log('javscript connected');
+
+const songs = [
+  "bensound-clearday.mp3",
+  "bensound-epic.mp3",
+  "bensound-funkyelement.mp3",
+  "bensound-goinghigher.mp3",
+  "bensound-retrosoul.mp3",
+  "bensound-summer.mp3",
+];
+
+const createSongList = () => {
+  const list = document.createElement('ol');
+  songs.forEach((song, i, songs) => {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode(song));
+    list.appendChild(item);
+  });
+  return list;
+}
+
+document.querySelector('#songList').appendChild(createSongList());
